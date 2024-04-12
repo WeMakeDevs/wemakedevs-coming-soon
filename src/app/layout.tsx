b9a5f029d48db4d15766f33ac7ce24a4d66d7614
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-04-13 02:06:34 +0530

    feat: add Analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={spaceGrotesk.className}>{children}</body>
+            <body className={spaceGrotesk.className}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
