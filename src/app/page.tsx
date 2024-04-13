7ee17219582b5edf902b9999729608e1efbf99ea
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-04-13 21:07:11 +0530

    feat: add more links and replace text with icons
    add simple icons react package
    add more links in the footerLinks array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,57 @@
 import Image from "next/image";
 import logo from "@/assets/logo.svg";
 import Link from "next/link";
+import {
+    SiInstagram,
+    SiX,
+    SiLinkedin,
+    SiDiscord,
+    SiTelegram,
+    SiYoutube,
+    IconType,
+} from "@icons-pack/react-simple-icons";
+import { Fragment } from "react";
+
+type FooterLink = {
+    name: string;
+    url: string;
+    icon: IconType;
+};
 
 export default function Home() {
-    const footerLinks = [
+    const footerLinks: FooterLink[] = [
         {
             name: "Twitter",
             url: "https://twitter.com/wemakedevs",
+            icon: SiX,
         },
         {
             name: "LinkedIn",
             url: "https://linkedin.com/company/wemakedevs",
+            icon: SiLinkedin,
+        },
+        {
+            name: "Discord",
+            url: "https://discord.gg/wemakedevs",
+            icon: SiDiscord,
+        },
+        {
+            name: "YouTube",
+            url: "https://youtube.com/@wemakedevs",
+            icon: SiYoutube,
+        },
+        {
+            name: "Telegram",
+            url: "https://t.me/WeMakeDevs",
+            icon: SiTelegram,
         },
         {
             name: "Instagram",
             url: "https://instagram.com/wemakedevs",
+            icon: SiInstagram,
         },
     ];
+
     return (
         <div className='min-h-screen flex flex-col bg-[#141413] text-white px-5 md:px-10 lg:px-20'>
             <main className='grow flex flex-col gap-20 lg:gap-24 justify-center items-center'>
@@ -55,40 +90,27 @@ export default function Home() {
                 >
                     kunal@wemakedevs.org
                 </Link>
-                <ul className='flex gap-4 items-center text-white'>
-                    <li>
-                        <Link
-                            href='https://twitter.com/wemakedevs'
-                            rel='noopener noreferrer'
-                            target='_blank'
-                            className='hover:text-[#caffc7] hover:underline'
-                        >
-                            Twitter
-                        </Link>
-                    </li>
-                    <li className='text-white/25 font-bold'>/</li>
-                    <li>
-                        <Link
-                            href='https://linkedin.com/company/wemakedevs'
-                            rel='noopener noreferrer'
-                            target='_blank'
-                            className='hover:text-[#caffc7] hover:underline'
-                        >
-                            LinkedIn
-                        </Link>
-                    </li>
-                    <li className='text-white/25 font-bold'>/</li>
-                    <li>
-                        <Link
-                            href='https://instagram.com/wemakedevs'
-                            rel='noopener noreferrer'
-                            target='_blank'
-                            className='hover:text-[#caffc7] hover:underline'
-                        >
-                            Instagram
-                        </Link>
-                    </li>
-                </ul>
+                <div className='flex gap-4 items-center text-white justify-center'>
+                    {footerLinks.map((link, index) => (
+                        <Fragment key={index}>
+                            <Link
+                                href={link.url}
+                                aria-label={link.name}
+                                rel='noopener noreferrer'
+                                target='_blank'
+                            >
+                                <link.icon
+                                    title={link.name}
+                                    size={18}
+                                    color='#ffffff'
+                                />
+                            </Link>
+                            {index !== footerLinks.length - 1 && (
+                                <div className='text-white/25 font-bold'>/</div>
+                            )}
+                        </Fragment>
+                    ))}
+                </div>
             </footer>
         </div>
     );
